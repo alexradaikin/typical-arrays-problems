67a69ee18d00dee55f7e93ae9c348e68b504771e
alexradaikin <alex.radaikin@gmail.com>
2021-03-14 12:01:52 +0200

    init: start task Typical Arrays Problems

--- src/index.js
+++ src/index.js
@@ -1,12 +1,41 @@
 
 exports.min = function min (array) {
-  return 0;
+    if (array === undefined || array.length === 0) {
+        return 0;
+    }
+
+    let minValue = array.reduce(function(previousValue, currentItem) {
+        if(previousValue < currentItem){
+            return previousValue;
+        }else{
+            return currentItem;
+        }
+    });
+    return minValue;
+  
 }
 
 exports.max = function max (array) {
-  return 0;
+    if (array === undefined || array.length === 0) {
+        return 0;
+    } 
+    let minValue = array.reduce(function(previousValue, currentItem) {
+        if(previousValue > currentItem){
+            return previousValue;
+        }else{
+            return currentItem;
+        }
+    });
+    return minValue;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    if (array === undefined || array.length === 0) {
+        return 0;
+    }
+    let avgValue = array.reduce(function(previousValue, currentItem) {
+        return previousValue + currentItem;
+    });
+
+    return avgValue / array.length ;
 }
